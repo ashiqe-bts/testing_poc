a02f4f9922bc9cceb00ed0893f097ddc9234e7b4
Ashiqe <ashiqe@behindthescene.app>
2026-03-16 10:18:27 +0530

    Add DTO validation decorators

--- src/dto/update-user.dto.ts
+++ src/dto/update-user.dto.ts
@@ -1,5 +1,17 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsNotEmpty, IsString, Min } from 'class-validator';
+
 export class UpdateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name!: string;
+
+  @Type(() => Number)
+  @IsInt()
+  @Min(0)
   age!: number;
+
+  @IsString()
+  @IsNotEmpty()
   designation!: string;
 }
